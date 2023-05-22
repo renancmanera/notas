d3a06834ed50cc55b127cec9a19198d65bb076ef
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2023-05-22 15:45:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 function nota() {
     var nota = prompt('Digite a sua nota de 0-100');
 
-    if (nota >= 90) {
-        alert('Sua nota foi A');
-
-
-    } else if (nota >= 80 && nota < 90) {
-        alert('Sua nota foi B');
-
-
-    } else if (nota >= 70 && nota < 80) {
-        alert('Sua nota foi C');
-
-
-    } else if (nota >= 60 && nota < 70) {
-        alert('Sua nota foi D');
-
-
+    if (nota >= 0 && nota <= 100) {
+        if (nota >= 90) {
+            alert('Sua nota foi A');
+        } else if (nota >= 80) {
+            alert('Sua nota foi B');
+        } else if (nota >= 70) {
+            alert('Sua nota foi C');
+        } else if (nota >= 60) {
+            alert('Sua nota foi D');
+        } else if (60 >= nota) {
+            alert('Sua nota foi F');
+        }
     } else {
-        alert('Sua nota foi F');
+        alert('Nota inválida! Digite um valor entre 0 e 100.');
     }
 }
-
